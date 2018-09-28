a6dcf3a8f258d899b15aad844ad6e89f8e5c79ce
alps <huyanzongbo@126.com>
2018-09-28 18:08:18 +0800

    do some changes
    try some new character in Angular 4

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
-
+interface Address{
+  province:string;
+  city:string;
+}
+interface Description{
+  color:string;
+  text:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-  title = 'my-app';
+  // title = 'Special Clock';
+  // description = {
+  //   color:'Blue',
+  //   text:'Nice'
+  // }
+  // address={
+  //   province:'福建',
+  //   city:'厦门'
+  // }
+  title:string;
+  address:Address;
+  description:Description;
+  showSkills:boolean;
+  skills:string[];
+  constructor(){
+    this.title = 'My o\'Clock';
+    this.address = {
+      province:'福建',
+      city:'厦门'
+    };
+    this.description = {
+      color:'Blue',
+      text:'Nice'
+    }
+    this.showSkills = true ;
+    this.skills = [
+      'Angular 1.x',
+      'Angular 2.x',
+      'Angular 4.x',
+      'Angular 6.x',
+    ]
+  }
+  toogleSkills(evt:any){
+    console.log(evt);
+    evt.stopPropagation();
+    this.showSkills = !this.showSkills ;
+  }
 }
